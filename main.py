a4328e1461ed3f2d693bfb3436060d3de13b59fc
flame-grilled-hotdog <125128804+flame-grilled-hotdog@users.noreply.github.com>
2024-08-31 12:57:37 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ import urllib.parse
 from dotenv import load_dotenv
 import datetime
 import unicodedata
+import csv
 
 # FastAPIのインスタンス作成
 app = FastAPI()
@@ -33,7 +34,7 @@ BEARER_TOKEN = os.getenv('BEARER_TOKEN')
 # グローバル変数でTwitterクライアントを保持
 client = None
 
-#情報取得関数
+##### 名字データ取得関数（通常） #####
 def get_surname_data(pagenum):
 
     # WebページのURLを指定
@@ -56,7 +57,7 @@ def get_surname_data(pagenum):
     random_table_num = random.choice(table_list)
 
     while True:
-    # 1～n2の範囲内でランダムな整数を生成
+    # 1～500の範囲内でランダムな整数を生成
         random_row_num = random.randint(1, 500)
         selector = f"#content > div.post > table:nth-child({random_table_num}) > tbody > tr:nth-child({random_row_num}) > td:nth-child(1)"
         element = soup.select_one(selector)
@@ -139,6 +140,120 @@ def get_surname_data(pagenum):
 
     return rank, surname, population, origin, surname_url_encode
 
+##### 名字データ取得関数（管理人厳選） #####
+def get_selected_surname_data():
+
+    ###CSVファイルを読み込み###
+    csv_file = 'selected_surname_data.csv'
+    selected_surname_list = []
+
+    with open(csv_file, 'r', encoding='utf-8') as file:
+        reader = csv.reader(file)
+    
+        for row in reader:
+            # 1行目のデータをリストに格納
+            selected_surname_list.extend(row)
+
+    ###苗字ページへアクセス###
+    surname = random.choice(selected_surname_list)
+    surname_url = f"https://myoji-yurai.net/searchResult.htm?myojiKanji={surname}"
+    response = requests.get(surname_url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    ###ランク・人口を取得###
+    n2 = 1
+    for i in range(20):
+        selector = f"#content > div:nth-child({i}) > p"
+        element = soup.select_one(selector)
+        if element is None:
+            continue
+        if '【全国順位】' in element.get_text():
+            n2 = i
+            break
+        i += 1
+
+    rank_population_selector = f"#content > div:nth-child({n2}) > p"
+    rank_population = soup.select_one(rank_population_selector).get_text(strip=True)
+
+    rank = re.search(r'】\s*((\d{1,3}(,\d{3})*|\d+)位)', rank_population)
+    rank = rank.group(1)
+    del_str = ','
+    rank = rank.replace(del_str,'')
+
+    population = re.search(r'((およそ\d{1,3}(,\d{3})*|\d+)人)', rank_population)
+    population = population.group(1)
+
+    ###由来を取得###
+    n3 = 1
+    for i in range(50):
+        selector = f"#content > div:nth-child({i}) > div.box > h4"
+        element = soup.select_one(selector)
+        if element is None:
+            continue
+        if '由来解説' in element.get_text():
+            n3 = i
+            break
+        i += 1
+
+    origin_selector = f"#content > div:nth-child({n3}) > div.box > div"
+    origin = soup.select_one(origin_selector).get_text(strip=True)
+    del_str = 'この名字について情報をお持ちの方は「みんなの名字の由来」に投稿いただくか(※無料会員登録が必要です)、「名字の情報を送る」よりお寄せください。'
+    origin = origin.replace(del_str,'')
+
+    ###読み方を取得###
+    n4 = 1
+    for i in range(15):
+        selector = f"#content > div:nth-child({i}) > p"
+        element = soup.select_one(selector)
+        if element is None:
+            continue
+        if '読み' in element.get_text():
+            n4 = i
+            break
+        i += 1
+
+    ### URLを取得 ###
+    encoded_name = urllib.parse.quote(surname)
+    surname_url_encode = f"https://myoji-yurai.net/searchResult.htm?myojiKanji={encoded_name}"
+
+    reading_selector = f"#content > div:nth-child({n4}) > p"
+    reading = soup.select_one(reading_selector).get_text(strip=True)
+    del_str = '【読み】'
+    reading = reading.replace(del_str,'')
+
+    ###ツイート用に各項目を整形###
+    rank = f"★管理人厳選苗字\n【ランク】{rank}"
+    surname = f"【苗字】{surname}（{reading}）"
+    population = f"【人口】{population}"
+    origin = f"【由来】{origin}"
+    surname_url_encode = f"{surname_url_encode}" 
+
+    #文字数チェック
+    lencheckitem = [rank, surname, population, surname_url_encode]
+    origin_strlen = 0
+    except_origin_strlen = 0
+    for i in range(4):
+        except_origin_strlen += get_east_asian_width_count(lencheckitem[i])
+        i += 1
+    strlen = except_origin_strlen + get_east_asian_width_count(origin)
+    print(strlen)
+    if strlen > 280:
+        print("文字数超過")
+
+        #文字数調整
+        max_length = 280 - except_origin_strlen
+        truncated_text = truncate_text_to_length(origin, max_length)
+        origin = truncated_text
+
+    # 結果を出力
+    print(rank)
+    print(surname)
+    print(population)
+    print(origin)
+    print(surname_url_encode)
+
+    return rank, surname, reading, population, origin, surname_url_encode
+
 # 文字数カウント関数
 def get_east_asian_width_count(text):
     count = 0
@@ -174,14 +289,25 @@ def tweet_scheduled_message():
     if client is None:
         print("Twitterクライアントが初期化されていません。")
         return
-    pagenum = random.randint(24, 79)
-    rank, surname, population, origin, surname_url_encode = get_surname_data(pagenum)
+    # 現在の時間を取得
+    current_time = datetime.now()
+    current_hour = current_time.hour
+    current_minute = current_time.minute
+
+    # XX時00分の投稿の場合、管理人厳選苗字をツイートする。
+    if 0 <= current_minute <= 10:
+        rank, surname, population, origin, surname_url_encode = get_selected_surname_data()
+    else:
+        pagenum = random.randint(24, 79)
+        rank, surname, population, origin, surname_url_encode = get_surname_data(pagenum)
+
     try:
         message = f"{rank}\n{surname}\n{population}\n{origin}\n{surname_url_encode}"
         client.create_tweet(text=message)
         print("ツイートが送信されました！")
     except Exception as e:
         print(f"ツイートエラー: {e}")
+
 
 # アクセストークン取得関数
 def accesstoken_scheduled_fetch(full_url):
